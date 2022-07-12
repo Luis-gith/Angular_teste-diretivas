e68c6eb4ae33a4728d1bd18bdedd6c64f5e613bb
Luis-gith <luisfelipebelasco@hotmail.com>
2022-07-12 08:11:35 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'diretivas-teste';
+export class AppComponent implements OnInit {
+  count = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    let intervalo = setInterval( ()=> {
+      this.count++;
+      if(this.count === 10) {
+        clearInterval(intervalo);
+      }
+    }, 1000 )
+  }
+
+
 }
