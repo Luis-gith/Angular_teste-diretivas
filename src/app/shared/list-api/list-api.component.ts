da6a4a2b4017e829645423fabb73bb32714d5596
Luis-gith <luisfelipebelasco@hotmail.com>
2022-07-13 18:24:29 -0300

    commit

---
+++ src/app/shared/list-api/list-api.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ListApiService } from '../services/list-api.service';
+
+@Component({
+  selector: 'app-list-api',
+  templateUrl: './list-api.component.html',
+  styleUrls: ['./list-api.component.css']
+})
+export class ListApiComponent implements OnInit {
+  pokemons = {};
+
+  constructor(private lista : ListApiService) { }
+
+  ngOnInit(): void {
+    this.getList();
+  }
+
+  getList(){
+  this.lista.getList().subscribe(resultado => {
+    this.pokemons = resultado.results;
+
+    console.log(this.pokemons);
+  })
+  }
+
+}
--- src/app/shared/services/list-api.service.ts
+++ src/app/shared/services/list-api.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export type ApiResponse = {
+  info: {},
+  results: {},
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListApiService {
+  apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=151%27';
 
-  constructor() { }
+
+  constructor(private httpClient:HttpClient) { }
+
+  getList():Observable<ApiResponse> {
+    return this.httpClient.get<ApiResponse>(this.apiURL);
+
+  }
 }
