20bfb272cb7ab3cd4d9fbf6656cd7c8580f77f11
Luis-gith <luisfelipebelasco@hotmail.com>
2022-07-13 15:35:48 -0300

    pasta shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HighlighterDirective } from './directives/highlighter.directive';
+import { HighlighterDirective } from './shared/directives/highlighter.directive';
 
 @NgModule({
   declarations: [
